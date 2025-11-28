7d30a2b6c5cb7d3e5ea0b8c08263c9647ad1b15c
MathieuAudibert <mathieu.audibert27@gmail.com>
2025-11-28 16:30:51 +0100

    feat(better): better api

--- api/send_rappel.py
+++ api/send_rappel.py
@@ -1,38 +1,57 @@
 import requests
 import os
-from dotenv import load_dotenv
+import json
 from datetime import datetime, timedelta
 from http.server import BaseHTTPRequestHandler
+# from dotenv import load_dotenv
+# load_dotenv()
 
-load_dotenv()
 def handler(request: BaseHTTPRequestHandler, response):
     TOKEN = os.getenv("TOKEN_DISCORD")
     ID_GROUPE = os.getenv("TARGET_GROUP_ID")
-    CONTENT = os.getenv("MESSAGE_CONTENT")
 
-    date_for_rappel = (datetime.now() + timedelta(days=1)).strftime('%Y-%m-%d')
-    
-    if not all([TOKEN, ID_GROUPE, CONTENT]):
-        error_message = "[ERROR]: missing env variables (TOKEN_DISCORD, TARGET_GROUP_ID, or MESSAGE_CONTENT)"
+    if not all([TOKEN, ID_GROUPE]):
+        error_message = "[ERROR]: missing env var (TOKEN_DISCORD or TARGET_GROUP_ID)"
         print(error_message)
         response.status = 500
         response.body = error_message
         return
+    
+    try:
+        body_length = int(request.headers.get('content-length', 0))
+        body = request.rfile.read(body_length)     
+        data = json.loads(body)
+        CONTENT = data.get('message')
+        
+    except Exception as e:
+        error_message = f"[ERROR]: message invalid - {e}"
+        print(error_message)
+        response.status = 400 
+        response.body = error_message
+        return
 
+    if not CONTENT:
+        error_message = "[ERROR]: messge should be in body"
+        print(error_message)
+        response.status = 400
+        response.body = error_message
+        return
+    
+    date_for_rappel = (datetime.now() + timedelta(days=1)).strftime('%Y-%m-%d')  
     url = f"https://discord.com/api/v9/channels/{ID_GROUPE}/messages"
-    headers = {"Authorization": f"{TOKEN}"}
+    headers = {"Authorization": f"{TOKEN}", "Content-Type": "application/json"}
+    
     prefix = f"@here **RAPPEL {date_for_rappel}** : "
-    data = {"content": prefix + CONTENT}
+    data_payload = {"content": prefix + CONTENT}
 
-    res = requests.post(url, headers=headers, json=data)
-
+    res = requests.post(url, headers=headers, json=data_payload)
     if res.status_code == 200:
-        log_message = f"[INFO]: msg sent successfully date - {date_for_rappel}"
+        log_message = f"[INFO]: msg sent - {date_for_rappel}"
         print(log_message)
         response.status = 200
         response.body = log_message
     else:
-        error_message = f"[ERROR]: failed to send msg - {res.text}"
+        error_message = f"[ERROR]: failed - {res.text}"
         print(error_message)
         response.status = 500
         response.body = error_message
